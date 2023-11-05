fbec3ba6c549733ef5abc4024363962b32fe9f14
Danil Melnikov <danil.olegovich.melnikov@inbox.ru>
2023-11-05 19:12:09 +0600

    fruits&vegatables

--- src/App.js
+++ src/App.js
@@ -26,8 +26,9 @@ class App extends React.Component {
         if (this.state.is_disabled.length) {
             return ""
         }
-
         let words = [...this.state.words]
+        let audio = new Audio(words[0][3]);
+        audio.play();
         words.shift()
         this.setState({words, is_disabled: 'disabled', user_value: ''})
 
@@ -37,16 +38,20 @@ class App extends React.Component {
         if (this.state.words.length) {
             let pair = this.state.words[0]
             return (
-                <div>
-                    <p className="left">Осталось: {this.state.words.length} слов</p>
-                    <h1>Перевод слова:</h1>
-                    <h2>{pair[1]}</h2>
-                    <input type="text" placeholder="пишите тут" value={this.state.user_value}
-                           onChange={this.onChange}/>
-                    <div className={`btn ${this.state.is_disabled}`} onClick={this.onSubmit}>
-                        следующие слово
+                <div className="App" style={{backgroundImage: `url('${pair[2]}')`}}>
+                    <div className="container">
+                        <div className="block">
+                            <p>Осталось: {this.state.words.length} слов</p>
+                            <h1>Перевод слова:</h1>
+                            <h2>{pair[1]}</h2>
+                            <input type="text" placeholder="пишите тут" value={this.state.user_value}
+                                onChange={this.onChange}/>
+                            <div className={`btn ${this.state.is_disabled}`} onClick={this.onSubmit}>
+                                следующие слово
+                            </div>
+                        </div>
                     </div>
-                </div>
+            </div>
             )
         }
 
@@ -59,13 +64,7 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <div className="App">
-                <div className="container">
-                    {this.render_content()}
-                </div>
-            </div>
-        );
+        return this.render_content();
     }
 }
 
